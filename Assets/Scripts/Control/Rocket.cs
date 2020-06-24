4b72fc1da57c9dd6540f3c12728f2750caa6d51d
AlecGrover <39810460+AlecGrover@users.noreply.github.com>
2020-06-23 18:21:04 -0700

    Further Progress
    * Improved Collision
     + Sound Effects
     + Music
     + Particle Effects
     + Lose/Win Conditions

--- Assets/Scripts/Control/Rocket.cs
+++ Assets/Scripts/Control/Rocket.cs
@@ -9,6 +9,11 @@ public class Rocket : MonoBehaviour
     [SerializeField] float boostSpeed = 10f;
     [Tooltip("Multiplier applied to base rotation rate of 60 degrees per second")]
     [SerializeField] float rotateMultiplier = 1f;
+    [SerializeField] AudioClip deathExplosion;
+    [SerializeField] AudioClip victoryPing;
+    [SerializeField] GameObject explosionVFX;
+    [SerializeField] GameObject victoryVFX;
+    bool victory = false;
     bool alive = true;
 
     // Start is called before the first frame update
@@ -50,41 +55,91 @@ public class Rocket : MonoBehaviour
         bool boostInput = CrossPlatformInputManager.GetButton("Jump");
         if (boostInput && alive)
         {
+            SetParticleSystem(true);
             Rigidbody rigidbody = GetComponent<Rigidbody>();
             Vector3 boostVector = new Vector3(0, boostSpeed, 0);
             rigidbody.AddRelativeForce(boostVector * Time.deltaTime);
-            //boostVector = transform.TransformDirection(boostVector);
-            //rigidbody.velocity += (boostVector * Time.deltaTime);
-            //Debug.Log(boostVector.ToString());
             AudioSource audioSource = GetComponent<AudioSource>();
             audioSource.volume = 0.3f;
         }
         else
         {
+            SetParticleSystem(false);
             AudioSource audioSource = GetComponent<AudioSource>();
             audioSource.volume = 0.0f;
         }
     }
 
+    private void SetParticleSystem(bool setActive)
+    {
+        BoostParticles boostParticles = FindObjectOfType<BoostParticles>();
+        if (!boostParticles) { return; }
+        boostParticles.SetBoostParticlesActive(setActive);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Objective"))
+        {
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+            if (!rigidbody)
+            {
+                return;
+            }
+            if (Mathf.Abs(rigidbody.velocity.y) <= 0.009 && Mathf.Abs(rigidbody.velocity.x) <= 0.009)
+            {
+                if (victory) { return; }
+                victory = true;
+                if (victoryPing)
+                {
+                    GameObject audioSourceObject = Instantiate(new GameObject(), transform.position, Quaternion.identity);
+                    audioSourceObject.AddComponent<AudioSource>();
+                    audioSourceObject.GetComponent<AudioSource>().PlayOneShot(victoryPing, 0.06f);
+                }
+                if (victoryVFX)
+                {
+                    Instantiate(victoryVFX, transform.position, Quaternion.identity);
+                }
+                StartCoroutine(ReloadLevel());
+            }
+        }
+    }
+
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Friendly"))
+        if (collision.gameObject.CompareTag("Objective"))
+        {
+            return;
+        }
+        else if (collision.gameObject.CompareTag("Friendly"))
         {
             //Debug.Log("Safe impact, ignoring...");
             return;
         }
         else
         {
-            StartCoroutine(Crash());
+            StartCoroutine(Crash(collision.contacts[0].point));
         }
     }
 
-    IEnumerator Crash ()
+    IEnumerator Crash(Vector3 collisionTransform)
     {
         alive = false;
         //Debug.Log("Bad impact");
         AudioSource audioSource = GetComponent<AudioSource>();
         audioSource.volume = 0.0f;
+        if (deathExplosion)
+        {
+            GameObject audioSourceObject = Instantiate(new GameObject(), transform.position, Quaternion.identity, transform);
+            audioSourceObject.AddComponent<AudioSource>();
+            audioSourceObject.GetComponent<AudioSource>().PlayOneShot(deathExplosion, 0.1f);
+        }
+        SetParticleSystem(false);
+        if (explosionVFX) {
+            GameObject explosion = Instantiate(explosionVFX, collisionTransform, Quaternion.identity, transform);
+            Destroy(explosion, 1f);
+        }
         yield return new WaitForSeconds(2.5f);
         Destroy(gameObject);
         LevelManager levelManager = FindObjectOfType<LevelManager>();
---
+++ Assets/Scripts/Particles/BoostParticles.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostParticles : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void SetBoostParticlesActive(bool activeState)
+    {
+        ParticleSystem particleSystem = GetComponent<ParticleSystem>();
+        if (particleSystem)
+        {
+            if (particleSystem.isPlaying == activeState)
+            {
+                return;
+            }
+            if (activeState)
+            {
+                particleSystem.Play();
+            }
+            else
+            {
+                particleSystem.Stop();
+            }
+        }
+    }
+
+
+}
